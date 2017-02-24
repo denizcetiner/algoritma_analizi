5fb7eff067f1e7f909e67203bbdabeca50eaf8dc
Deniz <Deniz@DESKTOP-IF0G3V9>
2017-02-24 14:12:04 +0300

    Add project files.

---
+++ algoritma_kuvvetalma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoritma_kuvvetalma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(power(3, 5));
+            Console.ReadKey();
+        }
+
+        public static int power(int a, int b)
+        {
+            int result = 1;
+            for(int i=0;i<b;i++)
+            {
+                result = result * a;
+            }
+            return result;
+        }
+    }
+}
